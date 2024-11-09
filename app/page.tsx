a2e802ffc45f48b3cb4e174231ca1d02033d24d6
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-11-09 19:08:09 +0100

    mapped mockData to home page with temp readable styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import { mockData } from '@/data/mockData';
 
 export default function Home() {
-  console.log(mockData);
   return (
     <div>
-      <main>
-        <h1>Dashboard</h1>
+      <main className="flex w-full items-center justify-center">
+        <div className="w-1/2">
+          {mockData.map((data) => (
+            <div className="flex justify-between border" key={data.id}>
+              <div>
+                <p>{data.fileName}</p>
+              </div>
+              <div className="flex space-x-4">
+                <p>{data.dateModified}</p>
+                <p>{data.size}</p>
+                <p>{data.type}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
