e78adbb9291af46abc6ea542edad1391347aebbd
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-11-10 12:55:56 +0100

    toggle view temp button and logic for list and grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
+'use client';
+
 import { files } from '@/data/files';
 import FilesList from '@/components/FilesList';
 import FilesGrid from '@/components/FilesGrid';
+import { useState } from 'react';
 
 export default function Home() {
+  const [view, setView] = useState('list');
+
+  const toggleView = () => {
+    setView(view === 'list' ? 'grid' : 'list');
+  };
+
   return (
     <div>
       <main className="flex w-full items-center justify-center">
         <section className="w-1/2">
-          <FilesList files={files} />
-          <FilesGrid files={files} />
+          <button
+            className="rounded-xl border bg-gray-200 p-2"
+            onClick={toggleView}
+          >
+            Toggle View
+          </button>
+          {view === 'list' ? (
+            <FilesList files={files} />
+          ) : (
+            <FilesGrid files={files} />
+          )}
         </section>
       </main>
     </div>
