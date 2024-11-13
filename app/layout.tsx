5fad8931aadc2a46bf652f1ab806fa6ff9625be7
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-11-13 22:46:21 +0100

    shadcn sidebar-04 install, deleted boilerplate, text and layout styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import { AppSidebar } from '@/components/app-sidebar';
+
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
   variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
   weight: '100 900',
 });
 
@@ -26,10 +28,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} bg-black antialiased`}>
+        <SidebarProvider
+          style={
+            {
+              '--sidebar-width': '19rem',
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar />
+          <SidebarInset className="bg-black py-2">
+            <header className="h-16 shrink-0 items-center gap-2 rounded-tl-lg bg-gray-50 px-4 pt-2">
+              <div className="flex">
+                <div className="mr-1 h-2 w-14 rounded-tl-full bg-teal-400" />
+                <div className="h-1 w-2 bg-teal-400" />
+              </div>
+              <div className="flex">
+                <div className="mr-1 h-1 w-14 bg-black" />
+                <div className="h-1 w-2 bg-black" />
+              </div>
+              <SidebarTrigger className="-ml-1 size-8" />
+            </header>
+            <div className="rounded-bl-lg bg-gray-50 text-sm">{children}</div>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,110 @@
+import * as React from 'react';
+import { Bot, GalleryVerticalEnd } from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from '@/components/ui/sidebar';
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: 'Implementation',
+      url: '/',
+      items: [
+        {
+          title: 'Dashboard',
+          url: '/',
+          isActive: true,
+        },
+      ],
+    },
+    {
+      title: 'Discussion',
+      url: '#',
+      items: [
+        {
+          title: 'AI Influence',
+          url: '#',
+          isActive: false,
+        },
+        {
+          title: 'Design and Interaction Patterns',
+          url: '#',
+          isActive: false,
+        },
+        {
+          title: 'Opportunities and Challenges',
+          url: '#',
+          isActive: false,
+        },
+        {
+          title: 'Implications and Directions',
+          url: '#',
+          isActive: false,
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="floating" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-12 items-center justify-center rounded-lg bg-teal-400 p-1">
+                  <Bot className="h-24 w-24" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="text-lg font-bold tracking-wide">
+                    Chatter Boxer
+                  </span>
+                  <span className="font-bold tracking-widest">Manager</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu className="gap-2">
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-bold">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+                {item.items?.length ? (
+                  <SidebarMenuSub className="ml-0 border-l-0 px-1.5">
+                    {item.items.map((item) => (
+                      <SidebarMenuSubItem key={item.title}>
+                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                          <a href={item.url}>{item.title}</a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                ) : null}
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
