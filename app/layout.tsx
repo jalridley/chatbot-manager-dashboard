0a3b35cc2638aeee13a9ca65b851952d7d8396be
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-11-13 23:46:14 +0100

    Bot avatar to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar';
+import { Bot } from 'lucide-react';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -39,15 +40,20 @@ export default function RootLayout({
           <AppSidebar />
           <SidebarInset className="bg-black py-2">
             <header className="h-16 shrink-0 items-center gap-2 rounded-tl-lg bg-gray-50 px-4 pt-2">
-              <div className="flex">
-                <div className="mr-1 h-2 w-14 rounded-tl-full bg-teal-400" />
-                <div className="h-1 w-2 bg-teal-400" />
+              <div className="flex justify-between">
+                <div>
+                  <div className="flex">
+                    <div className="mr-1 h-2 w-14 rounded-tl-full bg-teal-400" />
+                    <div className="h-1 w-2 bg-teal-400" />
+                  </div>
+                  <div className="flex">
+                    <div className="mr-1 h-1 w-14 bg-black" />
+                    <div className="h-1 w-2 bg-black" />
+                  </div>
+                  <SidebarTrigger className="-ml-1 size-8" />
+                </div>
+                <Bot className="h-10 w-10 rounded-full bg-teal-400 p-1.5 ring-2 ring-black" />
               </div>
-              <div className="flex">
-                <div className="mr-1 h-1 w-14 bg-black" />
-                <div className="h-1 w-2 bg-black" />
-              </div>
-              <SidebarTrigger className="-ml-1 size-8" />
             </header>
             <div className="rounded-bl-lg bg-gray-50 text-sm">{children}</div>
           </SidebarInset>
