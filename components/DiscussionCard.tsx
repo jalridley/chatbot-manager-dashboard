5ecb6a5f02d73dbfd4a5cbfa76cecc1d0136d1f9
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-11-14 12:48:51 +0100

    refactored discussion card

--- components/DiscussionCard.tsx
+++ components/DiscussionCard.tsx
@@ -1,18 +1,31 @@
 import Image from 'next/image';
 
+interface Content {
+  subheader: string;
+  text: string[];
+}
+
 interface Topic {
   title: string;
   question: string;
-  content: string[];
+  content: Content[];
   imageUrl: string;
   altText: string;
   width: number;
   height: number;
 }
 
-const DiscussionCard = ({ topic }: { topic: Topic }) => {
+interface DiscussionCardProps {
+  topic: Topic;
+}
+
+const renderTextList = (text: string[] = []) => {
+  return text.map((bullet) => <li key={bullet}>{bullet}</li>);
+};
+
+const DiscussionCard = ({ topic }: DiscussionCardProps) => {
   return (
-    <article className="grid items-center justify-items-center gap-4 overflow-hidden rounded-lg bg-white shadow-lg md:grid-cols-2 md:gap-12">
+    <article className="grid items-center overflow-hidden rounded-lg bg-white shadow-lg md:grid-cols-2">
       <Image
         src={topic.imageUrl}
         alt={topic.altText}
@@ -20,16 +33,16 @@ const DiscussionCard = ({ topic }: { topic: Topic }) => {
         height={topic.height}
         className="aspect-square h-full w-full object-cover"
       />
-      <div className="flex flex-col justify-center space-y-4 p-6">
+      <div className="flex flex-col justify-center space-y-4 p-4 md:p-8">
         <h2 className="text-xl font-semibold">{topic.title}</h2>
-        <h3 className="text-gray-600">{topic.question}</h3>
-        <ul className="list-disc pl-6">
-          {topic.content.map((bullet) => (
-            <li key={bullet} className="text-gray-600">
-              {bullet}
-            </li>
-          ))}
-        </ul>
+        <h3>{topic.question}</h3>
+
+        {topic.content.map((content, index) => (
+          <div key={index} className="space-y-2">
+            <h4 className="font-medium">{content.subheader}</h4>
+            <ul className="list-disc pl-6">{renderTextList(content.text)}</ul>
+          </div>
+        ))}
       </div>
     </article>
   );
